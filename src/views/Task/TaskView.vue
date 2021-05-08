8dccbba351079826e33f3a23a4768dcf0e0e8a7f
ShootingersNew <lebedevseergey@gmail.com>
2021-05-08 09:40:18 +0300

    add taskView

---
+++ src/views/Task/TaskView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="taskView">
+    <v-container>
+      <h1 class="mr-3">Задание № {{ taskNumber }}</h1>
+    </v-container>
+    <v-divider></v-divider>
+    <v-container>
+      <slot></slot>
+    </v-container>
+  </div>
+</template>
+
+<script src="./TaskView.ts"></script>
