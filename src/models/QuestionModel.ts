82f4fc3a16ec82cabe8c3de239e58d8a55589fc6
ShootingersNew <lebedevseergey@gmail.com>
2021-05-09 21:50:33 +0300

    add default props for question model

--- src/models/QuestionModel.ts
+++ src/models/QuestionModel.ts
@@ -2,11 +2,14 @@ import { AnswerModel } from "./AnswerModel";
 
 type typesUnion = "one" | "multiple";
 export class QuestionModel {
-  title: string;
-  type: typesUnion;
-  types: { title: string; id: typesUnion }[];
-  answers: AnswerModel[];
-  correctAnswers: number[];
+  title = "";
+  type: typesUnion = "one";
+  types: { title: string; id: typesUnion }[] = [
+    { id: "one", title: "Одиночный выбор" },
+    { id: "multiple", title: "Множественный выбор" },
+  ];
+  answers: AnswerModel[] = [];
+  correctAnswers: number[] = [0];
   constructor(init?: Partial<QuestionModel>) {
     if (init) {
       Object.assign(this, init);
