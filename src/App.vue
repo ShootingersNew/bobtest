03e76bfac78d4b195e5c6551784ff09ddfbe9f37
ShootingersNew <lebedevseergey@gmail.com>
2021-05-11 09:35:35 +0300

    remove useless code & links

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark> Header </v-app-bar>
-
+    <v-app-bar app color="red" dark>
+      <div class="title">
+        <router-link class="ml-5" to="/">Домашняя</router-link>
+      </div>
+      <router-link class="ml-5" to="/second/editsurveys"
+        >На второе задание</router-link
+      >
+    </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
